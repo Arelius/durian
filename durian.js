c73c29df112b6b5825c48fe10292e9f7daa6e497
Nicholas "Indy" Ray <Arelius@gmail.com>
2013-01-07 21:08:07 -0800

    Updated to take step sizes in two directions into account.

--- durian.js
+++ durian.js
@@ -1,9 +1,9 @@
-function gon(ctx, x, y, halfWidth, halfHeight) {
+function gon(ctx, x, y, d1X, d1Y, d2X, d2Y) {
   ctx.beginPath();
   ctx.moveTo(x, y);
-  ctx.lineTo(x + halfWidth, y + halfHeight);
-  ctx.lineTo(x + halfWidth * 2, y);
-  ctx.lineTo(x + halfWidth, y - halfHeight);
+  ctx.lineTo(x + d1X, y + d1Y);
+  ctx.lineTo(x + d1X + d2X, y + d1Y - d2Y);
+  ctx.lineTo(x + d2X, y - d2Y);
   ctx.fill();
 }
 
@@ -11,67 +11,78 @@ function init() {
   var canvas = document.getElementById('scene');
   var ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-
   var hW = 40;
   var hH = 15;
   var sH = 5
   var sX = 200;
   var sY = 200;
+  var d1X = hW;
+  var d1Y = hH;
+  var d2X = hW;
+  var d2Y = hH;
+
+  d2X *= 0.7;
+  d2Y *= 0.7;
 
   function inc(W, H) {
     sX += W;
     sY += H;
   }
 
-  ctx.fillStyle = "rgb(200,0,0)";
+  ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
   
   // Start
   
-  gon(ctx, sX, sY, hW, hH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
   // Up and Right
 
-  inc(hW, -hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(d2X, -d2Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(hW, -hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(d2X, -d2Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(hW, -hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(d2X, -d2Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
   // Up and Left
 
-  inc(-hW, -hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(-d1X, -d1Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(-hW, -hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(-d1X, -d1Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(-hW, -hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(-d1X, -d1Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
+
+  //hH += 10;
+  //hW += 10;
+
+  //hH *= 1.3;
+  //hW *= 1.3;
 
   // Down and Left
 
-  inc(-hW, + hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(-d2X, + d2Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(-hW, + hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(-d2X, + d2Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(-hW, + hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(-d2X, + d2Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
   // Down and Right
 
-  inc(hW, + hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(d1X, + d1Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(hW, + hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(d1X, + d1Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
 
-  inc(hW, + hH - sH);
-  gon(ctx, sX, sY, hW, hH);
+  inc(d1X, + d1Y - sH);
+  gon(ctx, sX, sY, d1X, d1Y, d2X, d2Y);
  
 }
